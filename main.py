2922fca4135a3caab576ab74cd63b9af02a35504
evelynod <odonnell.ev@gmail.com>
2025-08-10 20:55:07 -0700

    Add loop that draws black screen

--- main.py
+++ main.py
@@ -3,15 +3,32 @@
 # throughout this file
 import pygame
 from constants import *
-
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #2.12
 
 
 
 
 def main():
+    pygame.init()   # I'm not sure if the () should have anything in it.  Possibly (numpass, numfail)2-12
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    #import pygame
+
+
+
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return  # This will break the loop safely  Boot's suggested "running == False"
+
+        screen.fill((0, 0, 0))     # Fill the screen with black
+        pygame.display.flip()      # Update the display
+
+    pygame.quit()
 
 
 if __name__ == "__main__":
