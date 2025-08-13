b683c4b88cc200dfa47048619d9596b27250c27c
evelynod <odonnell.ev@gmail.com>
2025-08-12 18:39:29 -0700

    Add & import PLAYER_TURN_SPEED,& add rotate method to Player class

--- player.py
+++ player.py
@@ -1,10 +1,11 @@
 import pygame                # Not sure if I need this.  Hello, Boots?  I hope you are having a nice day.  Do I need import pygame?
 from circleshape import CircleShape
 from constants import PLAYER_RADIUS
+from constants import PLAYER_TURN_SPEED 
 
 class Player(CircleShape):    
     def __init__(self, x, y):                  #  The Player constructor should take x and y integers as input, then:
-        super().__init(x,y,PLAYER_RADIUS)      # Call the parent class's constructor, also passing in PLAYER_RADIUS
+        super().__init__(x,y,PLAYER_RADIUS)      # Call the parent class's constructor, also passing in PLAYER_RADIUS
         self.rotation = 0                           # Create a field called rotation, initialized to 0 (What is a field?)
         self.x = x                                  # Boots answered that a field is another name for instance variable, and needs self.--
         self.y = y
@@ -34,6 +35,10 @@ class Player(CircleShape):
         triangle_points = self.triangle()
         line_width = 2
         pygame.draw.polygon(screen, draw_color, triangle_points, line_width)  
-
-        
         return       
+    
+    # Add a new method to the Player class called rotate.  It's going to take one argument: dt.
+    # When it's called, it should add (PLAYER_TURN_SPEED * dt) to the player's current rotation.
+
+    def rotate(self, dt):
+        return (PLAYER_TURN_SPEED * dt) + self.rotation
